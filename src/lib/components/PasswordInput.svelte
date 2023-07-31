06b20e917d3f856ca1d918269e9a19c498f04e14
pitzzahh <pitzzahh@gmail.com>
2023-07-31 13:11:33 +0800

    chore: fix toggle and its alignment

--- src/lib/components/PasswordInput.svelte
+++ src/lib/components/PasswordInput.svelte
@@ -2,7 +2,7 @@
 	import eye from '$lib/assets/auth/eye.svg';
 	import eyeClosed from '$lib/assets/auth/eye-closed.svg';
 	export let togglePassword: boolean;
-    export let content: string = '';
+	export let content: string = '';
 
 	$: inputType = togglePassword ? 'text' : 'password';
 </script>
@@ -13,13 +13,16 @@
 		type={inputType}
 		class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary block py-2.5 pl-2.5 pr-10 bg-transparent dark:border-gray-600 w-full dark:placeholder-gray-400 dark:text-slate-100 dark:focus:ring-blue-500 dark:focus:border-priamry"
 		name="password"
-        value={content}
+		value={content}
 	/>
-	<button on:click|preventDefault class="absolute top-1/2 right-2 -translate-y-1/2">
+	<button
+		on:click|preventDefault
+		class="absolute {togglePassword ? 'top-1/2' : 'top-[60%]'} right-2 -translate-y-1/2"
+	>
 		{#if togglePassword}
-			<img width="30" height="30" src={eyeClosed} alt="Toggle hide password icon" />
+			<img width="30" height="30" src={eye} alt="Toggle show password icon" />
 		{:else}
-			<img width="30" height="30" src={eye} alt="Toggle hide password icon" />
+			<img width="30" height="30" src={eyeClosed} alt="Toggle hide password icon" />
 		{/if}
 	</button>
 </div>
