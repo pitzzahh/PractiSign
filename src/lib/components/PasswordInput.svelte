7662243e8bf67231f88210b7b244fc5d9fda4195
pitzzahh <pitzzahh@gmail.com>
2023-08-02 14:24:10 +0800

    chore: removed toggle focus outline

--- src/lib/components/PasswordInput.svelte
+++ src/lib/components/PasswordInput.svelte
@@ -11,13 +11,13 @@
 	<input
 		required
 		type={inputType}
-		class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary block py-2.5 pl-2.5 pr-10 bg-transparent dark:border-gray-600 w-full dark:placeholder-gray-400 dark:text-slate-100 dark:focus:ring-blue-500 dark:focus:border-priamry"
+		class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary block py-2.5 pl-2.5 pr-10 bg-transparent dark:border-gray-600 w-full dark:placeholder-gray-400 dark:text-slate-100 dark:focus:primary dark:focus:border-priamry"
 		name="password"
 		value={content}
 	/>
 	<button
 		on:click|preventDefault
-		class="absolute {togglePassword ? 'top-1/2' : 'top-[60%]'} right-2 -translate-y-1/2"
+		class="absolute {togglePassword ? 'top-1/2' : 'top-[60%]'} right-2 -translate-y-1/2 focus:outline-none"
 	>
 		{#if togglePassword}
 			<img width="30" height="30" src={eye} alt="Toggle show password icon" />
