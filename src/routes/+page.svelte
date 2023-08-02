93e68a38d005c28ba49afb2c5123052b397927a0
pitzzahh <pitzzahh@gmail.com>
2023-08-02 14:28:42 +0800

    chore: add head and seo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,28 @@
 <svelte:head>
-	<title>PractiSign</title>
+	<title>PractiSign - Sign In and Registration Application</title>
 	<meta
 		name="description"
-		content="Main application"
+		content="Secure sign-in and registration application for PractiSign. Create an account, manage users, and enjoy smooth authentication."
 	/>
 	<meta name="author" content="Peter John Arao" />
-	<meta name="og:title" content="PractiSign" />
+	<meta name="keywords" content="sign-in application, registration app, secure authentication, user management, PractiSign" />
+	<meta name="robots" content="index, follow" />
+	<link rel="canonical" href="https://practi-sign.vercel.app/" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta http-equiv="Content-Language" content="en" />
+	<meta name="format-detection" content="telephone=no" />
+	<meta name="theme-color" content="#ffffff" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="default" />
+	<meta property="og:title" content="PractiSign - Sign In and Registration Application" />
+	<meta property="og:description" content="Secure sign-in and registration application for PractiSign. Create an account, manage users, and enjoy smooth authentication." />
+	<meta property="og:image" content="https://practi-sign.vercel.app/favicon.png" />
+	<meta property="og:url" content="https://practi-sign.vercel.app/" />
+	<meta property="og:type" content="website" />
 	<meta property="og:site_name" content="PractiSign" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="@PractiSignApp" />
+	<meta name="twitter:title" content="PractiSign - Sign In and Registration Application" />
+	<meta name="twitter:description" content="Secure sign-in and registration application for PractiSign. Create an account, manage users, and enjoy smooth authentication." />
+	<meta name="twitter:image" content="https://practi-sign.vercel.app/favicon.png" />
 </svelte:head>
