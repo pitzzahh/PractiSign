c5bba62049ee697b07bb8fb1f97fd22a09a69883
pitzzahh <pitzzahh@gmail.com>
2023-08-05 14:02:43 +0800

    chore: rename dir Authentication to auth

--- src/lib/auth/SignUp.svelte
+++ src/lib/auth/SignUp.svelte
@@ -1,62 +1,68 @@
 <script lang="ts">
 	import Button from '$lib/components/Button.svelte';
 	import PasswordInput from '$lib/components/PasswordInput.svelte';
-	import SignInOptions from './SignInOptions.svelte';
 	import signinBanner from '$lib/assets/auth/signin-md-banner-1.jpg';
 	import { scale } from 'svelte/transition';
 	import { enhance } from '$app/forms';
-
 	let togglePassword: boolean;
 	let passwordContent: string;
-
+	
 	const buttonInfo = {
-		isSigningIn: false,
-		info: 'SignIn'
+		isSigninUp: true,
+		showLoading: false,
+		info: 'SignUp'
 	};
 
-	const handleSignIn = () => {
-		buttonInfo.isSigningIn = true;
-		buttonInfo.info = 'Signing in';
+	const handleSignUp = () => {
+		buttonInfo.isSigninUp = true;
+		buttonInfo.showLoading = true;
+		buttonInfo.info = 'Signing up';
 		setTimeout(() => {
-			buttonInfo.isSigningIn = false;
-			buttonInfo.info = 'SignIn';
+			buttonInfo.isSigninUp = false;
+			buttonInfo.showLoading = false;
+			buttonInfo.info = 'SignUp';
 		}, 2000);
 	};
 
-	function init(el: HTMLInputElement): void {
-		el.focus();
-	}
-
-	$: showingPassword = togglePassword;
 </script>
 
 <svelte:head>
-	<title>Sign in</title>
+	<title>Sign up</title>
 	<meta
 		name="description"
-		content="Sign in form"
+		content="Sign up form"
 	/>
 	<meta name="author" content="Peter John Arao" />
-	<meta name="og:title" content="Sign in" />
+	<meta name="og:title" content="Sign up" />
 	<meta property="og:site_name" content="PractiSign" />
 </svelte:head>
 
 <section class="dark:bg-primary min-h-screen flex items-center justify-center">
 	<div
 		in:scale
-		class="dark:bg-secondary flex items-center justify-center rounded-2xl shadow-2xl px-10 py-7 md:px-10 md:py-5 gap-4"
+		class="dark:bg-secondary flex items-center justify-center rounded-2xl shadow-2xl px-10 py-4 md:px-10 md:py-5 gap-4"
 	>
 		<div class="flex flex-start items-start justify-center w-full flex-col">
-			<span class="font-bold text-4xl dark:text-slate-100">Welcome back</span>
-			<span class="font-bold text-xl dark:text-slate-100">Sign in to continue</span>
+			<span class="font-bold text-4xl dark:text-slate-100">Nice to meet you</span>
+			<span class="font-bold text-xl dark:text-slate-100">Sign up to continue with our service</span
+			>
 			<form class="flex flex-col gap-4 mt-10 w-full" use:enhance>
+				<div>
+					<label for="text" class="sr-only">Name</label>
+					<p class="dark:text-slate-100 text-md font-bold mb-1">Name</p>
+					<input
+						required
+						type="text"
+						class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary block p-2.5 bg-transparent dark:border-gray-600 w-full dark:placeholder-gray-400 dark:text-slate-100 dark:focus:primary dark:focus:border-priamry"
+						name="text"
+					/>
+				</div>
 				<div>
 					<label for="email" class="sr-only">Email</label>
 					<p class="dark:text-slate-100 text-md font-bold mb-1">Email</p>
 					<input
 						required
 						type="text"
-						use:init
 						class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary block p-2.5 bg-transparent dark:border-gray-600 w-full dark:placeholder-gray-400 dark:text-slate-100 dark:focus:primary dark:focus:border-priamry"
 						name="email"
 					/>
@@ -64,22 +70,21 @@
 				<div>
 					<label for="password" class="sr-only">Password</label>
 					<p class="dark:text-slate-100 text-md font-bold mb-1">Password</p>
-					<PasswordInput {togglePassword} content={passwordContent}  on:click={() => (togglePassword = !togglePassword)} />
+					<PasswordInput {togglePassword} content={passwordContent} on:click={() => (togglePassword = !togglePassword)} />
 				</div>
 				<Button
 					info={buttonInfo.info}
-					on:click={handleSignIn}
-					showLoading={buttonInfo.isSigningIn}
+					on:click={handleSignUp}
+					showLoading={buttonInfo.showLoading}
 					styles="text-slate-100 bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-300 font-medium rounded-lg text-md px-5 py-2.5 
               dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
 				/>
-				<SignInOptions />
 				<hr class="h-[1px] dark:bg-[#24292F]" />
 				<div class="flex items-center justify-center gap-3">
-					<p class="dark:text-slate-100">Don't have an account?</p>
-					<a href="/signup">
+					<p class="dark:text-slate-100">Already have an account?</p>
+					<a href="/signin">
 						<Button
-							info="SignUp"
+							info="SignIn"
 							showLoading={false}
 							styles="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
 						/>
--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-    import SignUp from "$lib/Authentication/SignUp.svelte";
+    import SignUp from "$lib/auth/SignUp.svelte";
 </script>
 
 <SignUp/>
