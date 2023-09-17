d68f4cb4357e7a044fc1e0ea9d80944f3eba2a2d
pitzzahh <pitzzahh@gmail.com>
2023-09-17 14:22:20 +0800

    chore: add more state and authUser state

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,22 +1,30 @@
+import { redirect } from '@sveltejs/kit';
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import { writable } from 'svelte/store';
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyAV7Pw0n0xQkzrPq5zPM1fz1utpkGoEOaY',
-	authDomain: 'practisign.firebaseapp.com',
-	projectId: 'practisign',
-	storageBucket: 'practisign.appspot.com',
-	messagingSenderId: '614639788222',
-	appId: '1:614639788222:web:49fd13d45575611bf4feef'
+  apiKey: "AIzaSyAV7Pw0n0xQkzrPq5zPM1fz1utpkGoEOaY",
+  authDomain: "practisign.firebaseapp.com",
+  projectId: "practisign",
+  storageBucket: "practisign.appspot.com",
+  messagingSenderId: "614639788222",
+  appId: "1:614639788222:web:49fd13d45575611bf4feef"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-export const store = writable({});
+
+export const store = writable({
+	loggedIn: false,
+  isSigninIn: false,
+  togglePassword: false
+});
+
+export const authUser = writable<AuthUser | undefined>(undefined);
+
