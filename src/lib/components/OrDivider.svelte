a22d77dcb86a9ea290a17aa26c922e3028365542
pitzzahh <pitzzahh@gmail.com>
2023-09-17 14:27:53 +0800

    chore: add divider component

---
+++ src/lib/components/OrDivider.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let content: string = 'or'
+</script>
+
+<div class="flex items-center justify-center gap-2">
+	<hr class="w-1/2 h-[1px] dark:bg-[#24292F]" />
+	<p class="dark:text-slate-100 mb-1">{content}</p>
+	<hr class="w-1/2 h-[1px] dark:bg-[#24292F]" />
+</div>
+
