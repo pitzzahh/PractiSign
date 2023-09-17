df50aac4f64d9221f6c3f61b5145f3a12cc46f7d
pitzzahh <pitzzahh@gmail.com>
2023-09-17 14:28:40 +0800

    chore: remove server pages

--- src/routes/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-
-// TODO: check if logged in, if not redirect to login else redirect to dashboard
-// Temporary stuff, still learning sveltekit
-
-export function load() {
-	throw redirect(307, '/signin');
-}
