ebebb611f3804b90834a6022e6a4626e24b288cd
pitzzahh <pitzzahh@gmail.com>
2023-09-17 14:29:06 +0800

    chore: add basic signed in info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,21 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { auth, authUser } from '$lib';
+	import type { FirebaseError } from 'firebase/app';
+	import { signOut } from 'firebase/auth';
+
+	function handleSignOut(_event: MouseEvent & { currentTarget: EventTarget & HTMLButtonElement }) {
+		signOut(auth)
+			.then(() => {
+				$authUser = undefined;
+				goto('/signin');
+			})
+			.catch((error: FirebaseError) => {
+				console.log(error);
+			});
+	}
+</script>
+
 <svelte:head>
 	<title>PractiSign - Sign In and Registration Application</title>
 	<meta
@@ -5,7 +23,10 @@
 		content="Secure sign-in and registration application for PractiSign. Create an account, manage users, and enjoy smooth authentication."
 	/>
 	<meta name="author" content="Peter John Arao" />
-	<meta name="keywords" content="sign-in application, registration app, secure authentication, user management, PractiSign" />
+	<meta
+		name="keywords"
+		content="sign-in application, registration app, secure authentication, user management, PractiSign"
+	/>
 	<meta name="robots" content="index, follow" />
 	<link rel="canonical" href="https://practi-sign.vercel.app/" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -15,7 +36,10 @@
 	<meta name="apple-mobile-web-app-capable" content="yes" />
 	<meta name="apple-mobile-web-app-status-bar-style" content="default" />
 	<meta property="og:title" content="PractiSign - Sign In and Registration Application" />
-	<meta property="og:description" content="Secure sign-in and registration application for PractiSign. Create an account, manage users, and enjoy smooth authentication." />
+	<meta
+		property="og:description"
+		content="Secure sign-in and registration application for PractiSign. Create an account, manage users, and enjoy smooth authentication."
+	/>
 	<meta property="og:image" content="https://practi-sign.vercel.app/favicon.png" />
 	<meta property="og:url" content="https://practi-sign.vercel.app/" />
 	<meta property="og:type" content="website" />
@@ -23,6 +47,12 @@
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:site" content="@PractiSignApp" />
 	<meta name="twitter:title" content="PractiSign - Sign In and Registration Application" />
-	<meta name="twitter:description" content="Secure sign-in and registration application for PractiSign. Create an account, manage users, and enjoy smooth authentication." />
+	<meta
+		name="twitter:description"
+		content="Secure sign-in and registration application for PractiSign. Create an account, manage users, and enjoy smooth authentication."
+	/>
 	<meta name="twitter:image" content="https://practi-sign.vercel.app/favicon.png" />
 </svelte:head>
+
+<h1>Hello welcome {auth.currentUser?.displayName}</h1>
+<button on:click={handleSignOut}>SignOut</button>
