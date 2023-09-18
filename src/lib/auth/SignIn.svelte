486ac17a8a6d043b8b2f66df9a723833c3834ff6
pitzzahh <pitzzahh@gmail.com>
2023-09-18 20:26:17 +0800

    chore: add scale on hover to btn

--- src/lib/auth/SignIn.svelte
+++ src/lib/auth/SignIn.svelte
@@ -99,7 +99,7 @@
 					<p class="dark:text-slate-100">Don't have an account?</p>
 					<Button
 						on:click={() => goto('/signup')}
-						styles="focus:outline-none text-white bg-green-700 hover:bg-green-800  font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700"
+						styles="focus:outline-none text-white bg-green-700 hover:bg-green-800  font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700 hover:scale-105"
 					>
 						SignUp
 					</Button>
--- src/lib/auth/SignUp.svelte
+++ src/lib/auth/SignUp.svelte
@@ -137,7 +137,7 @@
 					<p class="dark:text-slate-100">Already have an account?</p>
 					<Button
 						on:click={() => goto('/signin')}
-						styles="focus:outline-none text-white bg-green-700 hover:bg-green-800  font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700"
+						styles="focus:outline-none text-white bg-green-700 hover:bg-green-800  font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700 hover:scale-105"
 					>
 						SignIn
 					</Button>
