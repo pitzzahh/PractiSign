ec07dec6e79eb71deb2b0cc7711cdeea45acc482
pitzzahh <pitzzahh@gmail.com>
2023-09-18 19:09:30 +0800

    chore: remove unecessary async

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,7 +2,7 @@ import { user } from '$lib';
 import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
-export const load = (async () => {
+export const load = (() => {
 	return new Promise<void>((resolve, reject) => {
 		user.subscribe((user) => {
 			console.log(`User on main page: ${user}`)
